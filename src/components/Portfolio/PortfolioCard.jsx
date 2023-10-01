7caf5997cf148c9b6ddebd3b6993ce22b55bef55
fahadhossain24 <fahadhossaim24@gmail.com>
2023-10-01 23:17:40 +0600

    update project with github and live link

--- src/components/Portfolio/PortfolioCard.jsx
+++ src/components/Portfolio/PortfolioCard.jsx
@@ -5,13 +5,13 @@ import { Icon } from '@iconify/react';
 const PortfolioCard = ({ portfolioDetails }) => {
     const { img, title, description, githubSourceCodeLink, projectLiveLink } = portfolioDetails
     return (
-        <div className='md:w-[30%] sm:w-[90%] border-[1px] border-[#ff6d5a] rounded-lg p-4 group'>
-            <img src={img} alt="avater" className='w-[100%] group-hover:scale-105 transition-all duration-300' />
+        <div className='md:w-[30%] sm:w-[90%] h-[530px] border-[1px] border-[#ff6d5a] rounded-lg p-4 group'>
+            <img src={img} alt="avater" className='w-[100%] h-[150px] group-hover:scale-105 transition-all duration-300' />
             <h2 className='text-center text-3xl font-bold p-4 bg-gradient-to-r from-blue-500 to-[#ff6d5a] text-transparent bg-clip-text group-hover:scale-110 transition-all duration-500'>{title}</h2>
-            <p className='text-white text-justify mb-5'>{description}</p>
+            <p className='h-[200px] text-[#bbb] text-justify mb-5'>{description}</p>
             <div className='lg:w-[100%] text-center sm:w-[100%]'>
-                <button><a href="" className='border-2 border-[#ff6d5a] text-xl px-5 py-[5px] text-[#ff6d5a] group-hover:bg-[#ff6d5a] group-hover:text-white transition-all duration-500 flex sm:mb-5 items-center gap-2 rounded-md'>Github <Icon icon="line-md:github-loop" /></a></button>
-                <button><a href="" className='border-2 border-[#ff6d5a] text-xl px-5 py-[5px] text-[#ff6d5a] group-hover:bg-[#ff6d5a] group-hover:text-white transition-all duration-500 flex items-end rounded-md ml-2'>See Details <Icon icon="material-symbols:arrow-right" className='text-2xl' /></a></button>
+                <button><a href={githubSourceCodeLink} target="_blank" className='border-2 border-[#ff6d5a] text-xl px-5 py-[5px] text-[#ff6d5a] group-hover:bg-[#ff6d5a] group-hover:text-white transition-all duration-500 flex sm:mb-5 items-center gap-2 rounded-md'>Github <Icon icon="line-md:github-loop" /></a></button>
+                <button><a href={projectLiveLink} target="_blank" className='border-2 border-[#ff6d5a] text-xl px-5 py-[5px] text-[#ff6d5a] group-hover:bg-[#ff6d5a] group-hover:text-white transition-all duration-500 flex items-end rounded-md ml-2'>Live Preview <Icon icon="material-symbols:arrow-right" className='text-2xl' /></a></button>
             </div>
         </div>
     );
--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import projectImg from '../../images/projectImg.jpg'
+import messProject from '../../images/messProject.png'
+import pharmacyProject from '../../images/pharmacyProject.png'
+import bikeProject from '../../images/bikeProject.png'
 import PortfolioCard from './PortfolioCard';
 import { useSpring, animated } from 'react-spring';
 import { useInView } from 'react-intersection-observer';
@@ -12,33 +14,41 @@ const Portfolio = () => {
         transform: inView ? 'translateY(1)' : 'translateY(80px)',
         config: { duration: 700 },
     });
+
+    const cardsInfo = [
+        {
+            img: messProject,
+            title: 'Bachelor Trip',
+            description: 'Bachelor Trip is a bachelor mess management system. Any person if her needed mess, then he can be find mess by any specific location or a person who want to create mess. He could be find mess and manage it by using this system. Mess admin each and everything control by using this system',
+            githubSourceCodeLink: 'https://github.com/fahadhossain24/my-mess.git',
+            projectLiveLink: 'https://my-mess-e50e9.web.app/'
+        },
+        {
+            img: bikeProject,
+            title: 'Expert Bike Service',
+            description: 'Expert Bike Service is a system where a user can be use in two approach. A bike rider have taken service for bike issue. Any king of bike problem, rider just come into this website then he can be take service. Other side a service men or mechanic register her service and he can be sell her service.',
+            githubSourceCodeLink: 'https://github.com/fahadhossain24/experts-bike-service-frontend.git',
+            projectLiveLink: 'https://expert-bike-service.web.app/'
+        },
+        {
+            img: pharmacyProject,
+            title: 'Medicine Park',
+            description: 'Medicine Park is a pharmacy management system. Where a pharmacist can entry all his medicines. If a customar asks about any medicine, he can search it and tell it along with its quantity and he can add all the types of medicine required by that customar to the card and finally he can print the slip',
+            githubSourceCodeLink: 'https://github.com/fahadhossain24/pharmacy-management.git',
+            projectLiveLink: 'https://medicine-park-29ca8.web.app/home'
+        }
+
+    ]
+
     return (
         <div id='protfolio' ref={ref} className='xl:w-[1280px] lg:w-[1024px] md:w-[768px] sm:w-[500px] mx-auto'>
             <div className='xl:w-[12%] lg:w-[15%] md:w-[25%] sm:w-[35%] mx-auto mt-20 mb-24'>
                 <h2 className='font-bold text-center text-3xl border-b-4 border-b-[#ff6d5a] pb-2 bg-gradient-to-b from-white to-[#ff6d5a] text-transparent bg-clip-text'>Portfolio</h2>
             </div>
             <animated.div className='flex flex-wrap justify-evenly' style={cardAnimation}>
-                <PortfolioCard portfolioDetails={{
-                    img: projectImg,
-                    title: 'Project title here',
-                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, reiciendis sapiente sunt quod laboriosam amet culpa pariatur. Optio assumenda vel sapiente aperiam, aliquam dolorem fuga veniam numquam. Dolore in est quam, eum soluta qui minus necessitatibus eveniet beatae dicta consectetur mollitia pariatur labore voluptas quasi repellendus eos optio id animi.',
-                    githubSourceCodeLink: '',
-                    projectLiveLink: ''
-                }}/>
-                <PortfolioCard portfolioDetails={{
-                    img: projectImg,
-                    title: 'Project title here',
-                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, reiciendis sapiente sunt quod laboriosam amet culpa pariatur. Optio assumenda vel sapiente aperiam, aliquam dolorem fuga veniam numquam. Dolore in est quam, eum soluta qui minus necessitatibus eveniet beatae dicta consectetur mollitia pariatur labore voluptas quasi repellendus eos optio id animi.',
-                    githubSourceCodeLink: '',
-                    projectLiveLink: ''
-                }}/>
-                <PortfolioCard portfolioDetails={{
-                    img: projectImg,
-                    title: 'Project title here',
-                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, reiciendis sapiente sunt quod laboriosam amet culpa pariatur. Optio assumenda vel sapiente aperiam, aliquam dolorem fuga veniam numquam. Dolore in est quam, eum soluta qui minus necessitatibus eveniet beatae dicta consectetur mollitia pariatur labore voluptas quasi repellendus eos optio id animi.',
-                    githubSourceCodeLink: '',
-                    projectLiveLink: ''
-                }}/>
+            {
+                cardsInfo.map((cardInfo, index) => <PortfolioCard key={index} portfolioDetails={cardInfo}/>)
+            }
             </animated.div>
         </div>
     );
