02b29fb946f1a80e7895b900e8ee7b5bd2c1716f
fahadhossain24 <fahadhossaim24@gmail.com>
2023-10-05 22:16:45 +0600

    add download resume button and click button then it will be downloaded

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -8,13 +8,18 @@ import Portfolio from '../Portfolio';
 import Reviews from '../Reviews';
 import GetInTouch from '../Get-in-touch';
 import Footer from '../../Shared/Footer';
+import resumePdf from '../../images/Fahad-Hossain-web-developer-Resume.pdf'
 
 const Home = () => {
+
+    const handleDownloadResume = () => {
+        console.log('clicke')
+    }
     return (
         <div id='home' className='bg-[#09072d] bg-gradient-to-br from-[#0c0b25] via-[#0c0b25] to-[#050266]'>
             <Header />
 
-            <div className='xl:w-[1280px] lg:w-[1024px] md:w-[768px] h-[650px] sm:w-[500px] mx-auto xl:relative'>
+            <div className='xl:w-[1280px] lg:w-[1024px] md:w-[768px] h-[650px] sm:w-[500px] mx-auto xl:relative sm:relative'>
                 <div className='xl:w-[50%] lg:w-[65%] md:w-[75%] sm:w-[95%] mx-auto p-8 my-radial-gradient rounded-full'>
                     <img src={fahadImg} alt="" className='w-[70%] lg:h-[550px] md:h-[500px] sm:h-[400px] ml-[90px] sm:ml-[70px]' />
                 </div>
@@ -42,9 +47,10 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className=''>
+                {/* resume download button */}
+                <div className='sm:absolute md:top-36 sm:top-14 lg:top-44  sm:ml-20 xl:ml-[520px] xl:mt-[-200px]'>
                     <div className='w-[200px] h-[100px] flex items-center justify-center mt-56'>
-                        <h2 className='resumeDownloadButton w-[85px] h-[85px] flex items-center justify-center text-6xl text-[#ff6d5a] border-2 border-[#ff6d5a] rounded-full p-3'><Icon icon="ic:baseline-download" className='resumeDownloadButtonIcon flex items-center justify-normal w-8' /></h2>
+                        <a href={resumePdf} download='Fahad-hossain-web-developer-resume' target='_blank' rel='noreferrer' className='resumeDownloadButton w-[85px] h-[85px] flex items-center justify-center text-6xl text-[#ff6d5a] border-2 border-[#ff6d5a] rounded-full p-3 cursor-pointer'><Icon icon="ic:baseline-download" className='resumeDownloadButtonIcon flex items-center justify-normal w-8' /></a>
                     </div>
                     <span className='resumeDownloadText text-[#ff6d5a] block mr-32 text-xl font-semibold mt-2 ml-5'>Download Resume</span>
                 </div>
