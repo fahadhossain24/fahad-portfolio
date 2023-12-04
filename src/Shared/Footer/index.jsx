5fa134fdc4ca8a8b702079e1dc67b906f687923f
Fahad Hossain <105900280+fahadhossain24@users.noreply.github.com>
2023-12-04 21:13:19 +0600

    Update index.jsx

--- src/Shared/Footer/index.jsx
+++ src/Shared/Footer/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
+    
     return (
         <div className='w-full text-center border-t-[1px] border-[#ff6d5a] p-6 text-2xl mt-16 text-white font-medium'>
             <p className='bg-gradient-to-b from-[#ff6d5a] to-blue-300 text-transparent bg-clip-text'>Fahad&copy;Hossain 2023</p>
